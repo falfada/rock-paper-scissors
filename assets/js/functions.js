57693eef245f17dd05466f6d49e74903a5265b0c
Daniela Leal <falfada@gmail.com>
2025-04-04 14:32:02 -0500

    Adding play again button

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,34 +1,69 @@
-// return what the user has chosen
-// alert("Welcome to Rock, Paper, Scissors!");
-
-
 let userInput = document.querySelectorAll(".user-input");
 const options = ["rock", "paper", "scissors"];
 
 const winningRules = {
-    rock: "paper",
-    paper: "scissors",
-    scissors: "rock",
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
+
+const itemsImage = {
+  rock: "assets/images/icon-rock.svg",
+  paper: "assets/images/icon-paper.svg",
+  scissors: "assets/images/icon-scissors.svg",
+};
+
+if (localStorage.getItem("score") === null) {
+  localStorage.setItem("score", 0);
 }
+const scoreDisplay = document.getElementById("scoreDisplay");
+currentScore = parseFloat(localStorage.getItem("score"));
+scoreDisplay.innerHTML = currentScore;
 
-for (const userChoice of userInput) {   
-    userChoice.addEventListener("click", function(){
-        let userChoiceValue = userChoice.value;
-        let computerChoice = options[Math.floor(Math.random() * options.length)];
-        console.log(`Your choice: ${userChoiceValue} and the computer choice: ${computerChoice} `);
-        if(userChoiceValue === computerChoice){
-            console.log("That's a tie!!");
-            // save the score
-            // play again
-        } else if(winningRules[userChoiceValue] === computerChoice){
-            console.log("You won!!");
-            // save the score
-            // play again 
-        } else{
-            console.log("You lost!!");
-            // save the score
-            // play again
-        }
+const displayResultsText = document.getElementById("displayResultsText");
+
+function updateScore(isWin) {
+  if (isWin) {
+    currentScore++;
+  } else if (currentScore > 0) {
+    currentScore--;
+  }
+  console.log(currentScore);
+  localStorage.setItem("score", currentScore);
+  scoreDisplay.innerHTML = currentScore;
+}
+for (const userChoice of userInput) {
+  userChoice.addEventListener("click", async function () {
+    let userChoiceValue = userChoice.value;
+    document.getElementById("userChoiceImg").src = itemsImage[userChoiceValue];
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    let computerChoice = options[Math.floor(Math.random() * options.length)];
+    document.getElementById("computerChoiceImg").src =
+      itemsImage[computerChoice];
+
+    let resultsMessage = document.createElement("p");
+    displayResultsText.appendChild(resultsMessage);
+
+    if (userChoiceValue === computerChoice) {
+      resultsMessage.innerHTML = "THAT'S A TIE";
+    } else if (winningRules[userChoiceValue] === computerChoice) {
+      resultsMessage.innerHTML = "YOU WIN";
+      updateScore(true);
+    } else {
+      resultsMessage.innerHTML = "YOU LOSE";
+      updateScore(false);
+    }
+    // play again button
+    let playAgainButton = document.createElement("BUTTON");
+    playAgainButton.setAttribute("id", "refreshButton");
+    playAgainButton.innerHTML = "PLAY AGAIN";
+    displayResultsText.appendChild(playAgainButton);
+
+    let refreshButton = document.getElementById("refreshButton");
+
+    refreshButton.addEventListener("click", function () {
+      location.reload();
     });
-
+  });
 }
